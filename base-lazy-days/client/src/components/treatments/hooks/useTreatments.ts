ddc0bc03698153a0d0952ffd9013b747367e2ccf
justinhwang92 <justinhwang92@gmail.com>
2022-10-14 15:40:10 -0400

    Add centralize loading and error handling

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,15 +1,31 @@
+import { useQuery } from 'react-query';
+
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
-import { useCustomToast } from '../../app/hooks/useCustomToast';
+// import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
-// async function getTreatments(): Promise<Treatment[]> {
-//   const { data } = await axiosInstance.get('/treatments');
-//   return data;
-// }
+async function getTreatments(): Promise<Treatment[]> {
+  const { data } = await axiosInstance.get('/treatments');
+  return data;
+}
 
 export function useTreatments(): Treatment[] {
-  // TODO: get data from server via useQuery
-  return [];
+  // const toast = useCustomToast(); // custom toast hook
+  const fallback = []; // for when we don't have data yet
+
+  // get data from server via useQuery
+  // Error handling with Passing errors to toasts
+  // const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
+  //   onError: (error) => {
+  //     const title =
+  //       error instanceof Error ? error.message : 'Error connecting to server';
+  //     toast({ title, status: 'error' }); // show toast on error
+  //   },
+  // });
+
+  // get data from server via useQuery
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
+  return data;
 }
